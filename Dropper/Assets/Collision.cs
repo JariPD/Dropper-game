5053715c73d1466d8357227ceb03f88bd23f5dc7
JariPD <200502@student.glu.nl>
2021-03-15 13:52:59 +0100

    collision en respawn scripts

---
+++ Dropper/Assets/Collision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Collision : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+   
+    private void OnControllerColliderHit(ControllerColliderHit collision)
+    {
+        //print("beep");
+        if (collision.gameObject.CompareTag("Obstacle1"))
+        {
+            print("collision detected 1");
+
+            transform.position = new Vector3(0, 10, 5);
+
+            rb.velocity = Vector3.zero;
+
+            Physics.SyncTransforms();
+        }
+
+        if (collision.gameObject.CompareTag("Obstacle2"))
+        {
+            print("collision detected 2");
+
+            transform.position = new Vector3(0, 10, 5);
+
+            rb.velocity = Vector3.zero;
+
+            Physics.SyncTransforms();
+        }
+
+        if (collision.gameObject.CompareTag("Obstacle3"))
+        {
+            print("collision detected 3");
+
+            transform.position = new Vector3(0, 10, 5);
+
+            rb.velocity = Vector3.zero;
+
+            Physics.SyncTransforms();
+        }
+    }     
+}
--- Dropper/Assets/Scripts/Respawn.cs
+++ Dropper/Assets/Scripts/Respawn.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-/*
-
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody rb;
 
     void Update()
     {
-        if (transform.position.x <= )
+        /*if (transform.position.x < -50)
         {
+            transform.position = new Vector3(0f, 5, 0f);
 
+            rb.velocity = Vector3.zero;
+
+            Physics.SyncTransforms();
         }
-    }*/
+
+        if (transform.position.x < 50)
+        {
+            transform.position = new Vector3(0f, 5, 0f);
+
+            rb.velocity = Vector3.zero;
+
+            Physics.SyncTransforms();
+        }
+
+        if (transform.position.y < 50)
+        {
+            transform.position = new Vector3(0f, 5, 0f);
+
+            rb.velocity = Vector3.zero;
+
+            Physics.SyncTransforms();
+        }*/
+    } 
 }
