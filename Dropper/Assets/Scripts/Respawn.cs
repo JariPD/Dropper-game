c6e7e7388c8ae61114740a48c471d35299b3c13b
JariPD <200502@student.glu.nl>
2021-03-15 13:06:32 +0100

    player

---
+++ Dropper/Assets/Scripts/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+/*
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.x <= )
+        {
+
+        }
+    }*/
+}
